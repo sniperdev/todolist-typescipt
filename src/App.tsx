3e7cb2a5a90881134663e97aa069c03ff3c27547
sniperdev <igorspychalaa@gmail.com>
2023-02-01 15:49:23 +0100

    <style> basic styling for whole page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="bg-black"></div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center bg-black">
+      <section className="w-1/3 h-2/3 border-2 rounded-2xl"></section>
+    </div>
+  );
 }
 
 export default App;
